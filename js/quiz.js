aa577c602854cf54d906777b8c116200024a85d3
Rucens <davemathew921@gmail.com>
2021-09-22 00:39:03 +1000

    Unlosable quiz created
    Created a quiz that just lets you win when you click submit. 
    Will take you to the certificate page with link

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,52 @@
 function prevPage(){
     location.href="article_p2.html";
 }
+
+function submit(){
+    win.style.display = "block";
+}
+/* $(function() {
+    var update = function() {
+        $('#serialize').text(        
+            JSON.stringify($('form').serialize())
+        );
+    };
+    $(result).innerHTML = check(update());
+    update();
+    $('form').change(update);
+})
+
+function check (update) {
+    var q1, q2, q3;
+    var total = 0;
+    const answers = update.split("&");
+    while(answers.localeCompare(update) != 0) {
+        console.log("hi");
+   while(answers.length > 0) {
+       var element = answers.pop();
+       var point = element.indexOf("=");
+       var question = element.substring(0, point);
+       var answer = element.substring(point, element.length - 1)
+       if(question.localeCompare("q1")) {
+           if(answer.localeCompare("a")){
+               total +=1;
+            }
+        }
+        else if(question.localeCompare("q2")) {
+            if(answer.localeCompare("b")){
+                total +=1;
+            }
+        }
+        else if(question.localeCompare("q3")) {
+                 if(answer.localeCompare("c")){
+                    total +=1;
+                }
+        }
+
+    }
+    answers = update.split("&");
+}
+    var send = total;
+    total=0;
+    return send;
+} */
