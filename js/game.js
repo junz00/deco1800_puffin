edbadb5b6e9eeb411102dbdb1216316b68a8af6d
Rucens <davemathew921@gmail.com>
2021-09-21 16:12:04 +1000

    Created drag game
    Created game where picture can be dragged and dropped onto image

--- js/game.js
+++ js/game.js
@@ -1,3 +1,18 @@
 function exitGame() {
     location.href="article.html";
 }
+
+function dragStart(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function showDrop(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    event.preventDefault();
+    var id = event.dataTransfer.getData("text");
+    var item = document.getElementById(id);
+    event.target.appendChild(item);
+}
