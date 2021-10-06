569805fbccca50745261d52bb13b47e4bd4a64e8
Nightingale <r.nightingale@uq.edu.au>
2021-10-06 16:29:12 +1000

    The API call to ABC News images is now working.

--- js/api-abc.js
+++ js/api-abc.js
@@ -1,3 +1,37 @@
+function getYear(year) {
+	if(year) {
+		return year.match(/[\d]{4}/); // This is regex (https://en.wikipedia.org/wiki/Regular_expression)
+	}
+}
+
+function iterateRecords(data) {
+	console.log(data);
+	$.each(data.result.records, function(recordKey, recordValue) {
+
+        var rImage = recordValue["Primary image"];
+        var rCaption = recordValue["Primary image caption"]
+		var rTitle = recordValue["Title"];
+		var rDate = recordValue["Date"];
+        var rLatitude = recordValue["Latitude"];
+        var rLongitude = recordValue["Longitude"];
+
+        console.log(rDate, rCaption, rImage);
+
+		if(/*recordTitle && recordDate && recordImage && recordDescription*/ true) {
+/*
+			$("#records").append(
+				$('<section class="record">').append(
+					$('<h2>').text(recordTitle),
+					$('<h3>').text(recordYear),
+					$('<img>').attr("src", recordImage),
+					$('<p>').text(recordDescription)
+				)
+			);
+*/
+		}
+	});
+}
+
 $(document).ready(function() {
 
     var data = {
@@ -6,14 +40,13 @@ $(document).ready(function() {
         q: "flood"
     };
 
-    console.log(data);
-
     $.ajax({
         url: "https://data.gov.au/data/api/3/action/datastore_search",
         data: data,
         dataType: 'jsonp',
+        cache: true,
         success: function(data) {
-            console.log(data)
+            iterateRecords(data);
         }
     });
 });
