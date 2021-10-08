ff7a9c3731ac5dd30dbb3556cb711e132bf0a4f0
Cassidy B <cassidy.bell@uqconnect.edu.au>
2021-10-09 02:25:24 +1000

    added scrap message on map pg

--- js/map.js
+++ js/map.js
@@ -5,3 +5,21 @@ function prevPage(){
 function nextPage(){
     location.href="article1_p1.php";
 }
+
+    //opens the event registration box
+    function openBox() {
+        var box = document.getElementById("scrapMessage");
+        box.style.display = "block";
+    }
+    //hides the box when the close button is pressed
+    function closeBox() {
+        var box = document.getElementById("scrapMessage");
+        box.style.display = "none";
+    }
+    //hides the box if the user presses outside of it        
+    window.onclick = function(event) {
+        var box = document.getElementById("scrapMessage");
+        var scrap = document.getElementById("scrap");
+        if (event.target == scrap) {
+            box.style.display = "none";}
+        }
--- map.php
+++ map.php
@@ -5,7 +5,19 @@
 <?php include("header.php"); ?>
 <?php include("bookmarks.php"); ?>
 
+<!--<body onload="openBox(this)">-->
 <div id="book-container">
+
+	<section id="scrapMessage">
+		<img id="scrap" src="images/paper_scrap.png" width=40%>
+		<div class="scrapText">
+			<span id="close"   onclick="closeBox()">&#10006;</span>
+			<p>This map of the Brisbane River shows your progress through the journal. <br>
+			Select a point on the map to begin that story or simply press next to 
+			begin the first article.</p>
+		</div>
+	</section>
+
 	<section id="book">
 		<img id="book-bg" src="images/diary_bg.jpg">
 		<div id="pages" class="spread">
