52823f04853255ad477b68665e5036faf2c585a9
junz00 <s4537565@student.uq.edu.au>
2021-10-13 06:36:37 +1000

    sandbag game

--- game.php
+++ game.php
@@ -5,29 +5,53 @@
 <?php include("bookmarks.php"); ?>
 
 	<div id="book-container">
-		<section id="book">
+		<section id="scrapMessage">
+			<img id="scrap" src="images/paper_scrap.png" width=40%>
+			<div class="scrapText">
+				<span id="close"   onclick="closeBox()">&#10006;</span>
+				<p>Welcome to the Sandbag Game! <br>
+					Use your knowledge of sandbags to help Aaron and his family protect their home. <br>
+					Remember, sandbag walls should be at least 2 sandbags high for maximum effectiveness. <br>
+					(Don't forget the plastic sheeting either!)
+				</p>
+			</div>
+		</section>
+		<section id = "book">
 			<img id="book-bg" src="images/diary_bg.jpg">
 			<div id="pages">
 				<div id="pagespread">
-					<img id="game_bg" src="images/grass.jpg" draggable="false">
+					<img id="game_bg" src="images/garage_bg.jpg" draggable="false">
 					<button type="button" id =exit_button onclick="exitGame()">exit</button>
 
 					<div id = "images">
-						<a href ="article1_p3.php">
+						<a href = "article1_p3.php">
 							<div id = "cont"><strong id = "cont_text"> Continue</strong></div>
 						</a>
+						
+						<div id = "plastic_cover"></div>
+						<div id = "box2" ondragover = "showDrop(event)" ondrop = "drop2(event)"></div>
+
+						<img id = "plastic_sheeting" src = "images/plastic_sheeting.png" draggable = "true" ondragstart = "dragStart1(event)">
 						<img id="dog_girl" src="images/dog_girl.jpg" draggable="true">
 						<img id="bernie" src="images/bernie.png" draggable="true">
 						<img id="fruits" src="images/fruits.png" draggable="true">
 
-						<div id = "box" ondragover="showDrop(event)" ondrop="drop(event)"> 
-							<p>Place Sandbag inside</p>
+						
+						
+						<div id = "box1" ondragover="showDrop(event)" ondrop="drop1(event)"> 
+							<p>Place objects here</p>
 						</div>
+						
+						
+
 						<img id="among_us" src="images/among_us.png" draggable="true">
-						<img id="sandbag" src="images/sandbag.png" draggable="true"	ondragstart="dragStart(event)">
+						<img id="sandbag1" src="images/sandbag.png" draggable="true" ondragstart="dragStart2(event)">
+						<img id="sandbag2" src="images/sandbag.png" draggable="true" ondragstart="dragStart2(event)">
+
 						<!--<button id="button-prev" onclick="prevPage()"> previous </button>
 						//<button id="button-next" onclick="nextPage()"> next </button>-->
 					</div>
+				</div>
 			</div>
 		</section>
 	</div>
--- js/game.js
+++ js/game.js
@@ -1,8 +1,10 @@
+var count = 0;
+
 function exitGame() {
     location.href="article1_p2.php";
 }
 
-function dragStart(event) {
+function dragStart1(event) {
     event.dataTransfer.setData("text", event.target.id);
 }
 
@@ -10,10 +12,50 @@ function showDrop(event) {
     event.preventDefault();
 }
 
-function drop(event) {
+function drop1(event) {
+    event.preventDefault();
+    var id = event.dataTransfer.getData("text");
+    if (id == "sandbag1" || id == "sandbag2") {
+        return;
+    }
+    var item = document.getElementById(id);
+    event.target.appendChild(item);
+    plastic_cover.style.display = "block";
+    box2.style.display = "block";
+    box1.style.display = "none";
+}
+
+function dragStart2(event) {
+    event.dataTransfer.setData("text", event.target.id); 
+}
+
+function drop2(event) {
     event.preventDefault();
     var id = event.dataTransfer.getData("text");
     var item = document.getElementById(id);
     event.target.appendChild(item);
-    cont.style.display = "block";
+
+    count = count + 1;
+    
+    if (count == 2) {
+        cont.style.display = "block";
+    }
 }
+
+//opens the event registration box
+function openBox() {
+    var box = document.getElementById("scrapMessage");
+    box.style.display = "block";
+}
+//hides the box when the close button is pressed
+function closeBox() {
+    var box = document.getElementById("scrapMessage");
+    box.style.display = "none";
+}
+//hides the box if the user presses outside of it        
+window.onclick = function(event) {
+    var box = document.getElementById("scrapMessage");
+    var scrap = document.getElementById("scrap");
+    if (event.target == scrap) {
+        box.style.display = "none";}
+    }
