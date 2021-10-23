6a6d454d81b607e78c51fc299bd0916e237b6d5f
Nightingale <r.nightingale@uq.edu.au>
2021-10-24 01:51:02 +1000

    More work on the gallery

--- js/gallery.js
+++ js/gallery.js
@@ -5,3 +5,15 @@ function prevPage(){
 function nextPage(){
     location.href="article1_p1.php";
 }
+
+$(document).ready(function() {
+    /* Event handler when changing galleries. */
+    $(".db-queries li").click(function() {
+        $(".db-queries li").removeClass("selected");
+        $(this).addClass("selected");
+        $(".gallery-images")
+                .attr("query", $(this).attr("query"))
+                .attr("limit", $(this).attr("limit"));
+        updateGalleries();
+    });
+});
