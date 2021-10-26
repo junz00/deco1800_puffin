aa4f471cf267681026dcbc9bfcf2d103a2f50154
Nightingale <r.nightingale@uq.edu.au>
2021-10-26 17:46:01 +1000

    Corrected the name of the website in the footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-	<p><strong><a href="index.php">Brisbane Flood Diary</a>: </strong>A project for DECO1800 - Interactive Design</p>
+	<p><strong><a href="index.php">Brisbane Flood Journal</a>: </strong>A project for DECO1800 - Interactive Design</p>
 	<p><a href="about.php">About this website</a></p>
 </footer>
 </body>
